cbfdcbbade03340d986a507df9f2c2ad63c8940f
Ian Chang <personal.ianchang@gmail.com>
2024-09-20 18:58:25 -0400

    testing with jupyter notebook

--- main.py
+++ main.py
@@ -1,35 +1,40 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from bs4 import BeautifulSoup
 import time
 
-chrome_driver_path = r"/Users/nyu/Documents/Ian's Folder/Misc/chromedriver-mac-arm64/chromedriver"
+# Set up options to use a custom User-Agent
+chrome_options = Options()
+chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
 
-# Step 1: Set up the WebDriver (in this case for Chrome)
+# Set up the WebDriver (in this case for Chrome)
 # You need to specify the path to your ChromeDriver
+chrome_driver_path = r"/Users/ianchang/Library/Mobile Documents/com~apple~CloudDocs/1. Project/chromedriver-mac-x64/chromedriver"
 
 # Initialize the WebDriver
 service = Service(chrome_driver_path)
 driver = webdriver.Chrome(service=service)
 
-# Step 2: Open the website
-url = 'https://www.ssense.com/en-us/men/bags?q=cdg'
+# Open the website
+url = 'https://www.artnet.com/auctions/all-artworks/'
 driver.get(url)
 
-# Optionally, wait for some seconds for the page to fully load
-time.sleep(5)  # You can adjust this depending on your connection
+time.sleep(5)
 
-# Step 3: Parse the page source using BeautifulSoup
+# Parse the page source using BeautifulSoup
 soup = BeautifulSoup(driver.page_source, 'html.parser')
 
-# Step 4: Extract the product data (adjust based on SSENSE's HTML structure)
+# Extract the product data (adjust based on SSENSE's HTML structure)
 # Example of finding product descriptions
-product_list = soup.find_all('div', class_='product-tile__description')
+product_list = soup.find_all('div', class_='details')
 
 # Print out the products
 for product in product_list:
     print(product.text)
 
-# Step 5: Close the WebDriver after scraping
+# Close the WebDriver after scraping
 driver.quit()
+
+print(product_list)
