aca2657ba2c49bb8fdc03f2ec05b6234dc65ad31
NYU <nyu@10-18-176-7.dynapool.wireless.nyu.edu>
2024-09-20 10:31:48 -0400

    virtual environment setup change

--- main.py
+++ main.py
@@ -2,5 +2,11 @@ import pandas as pd
 import sqlite4
 import datetime as datetime
 from bs4 import BeautifulSoup
+import os
+from dotenv import load_dotenv
 
-print("Hello World")
+# Load the .env file
+load_dotenv()
+
+# Access the API key
+forex_api_key = os.getenv("FOREX_API_KEY")
